453879e59cc3516e236ba67f1349762250073518
kotori <umi_kotori@outlook.com>
2023-01-27 18:25:56 +0800

    fixed modal dialog for items

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -1,46 +1,75 @@
 <template>
   <div id="contentBox">
-    <div class="item" v-for="item in items" :key="item.shop_id" @click="dialogVisible = true">
+    <div
+      class="item"
+      v-for="item in items"
+      :key="item.shop_id"
+      @click="dialogVisible = true"
+    >
       <h1>{{ item.name }}</h1>
       <img v-bind:src="'http://127.0.0.1:3007/api/pic/' + item.logo" alt="" />
-      <Dialog />
+      <!-- <Dialog /> -->
     </div>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="80%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import Dialog from './Dialog.vue';
+import Dialog from "./Dialog.vue";
 export default {
   data() {
     return {
+      dialogVisible: false,
       show: true,
-    }
+      items: [
+        {
+          shop_id: 1,
+          logo: 0,
+          name: "qwe",
+        },
+        {
+          shop_id: 2,
+          logo: 0,
+          name: "q1e",
+        },
+        {
+          shop_id: 3,
+          logo: 0,
+          name: "qwe",
+        },
+      ],
+    };
   },
 
   props: {
-    items: {
-      default: []
-    }
+    // items: {
+    //   default: []
+    // }
   },
   mounted() {
-    console.log(this.items)
+    console.log(this.items);
   },
 
   methods: {
     goto(where) {
       this.$router.replace(where);
     },
-    openModal () {
-        this.$modal.show('as-modal')
-      }
-
-
-
+    openModal() {
+      this.$modal.show("as-modal");
+    },
   },
 
   components: {
-    Dialog
-  }
+    Dialog,
+  },
 };
 </script>
 
@@ -60,6 +89,11 @@ export default {
   margin: 10px;
   background-color: #ffe6cf;
   border-radius: 10px;
+}
+
+.item:hover {
+  background-color: rgb(146, 123, 101);
+  transition: all 0.5s;
 }
 
 img {
